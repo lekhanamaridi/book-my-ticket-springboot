39c4320d33a92de097c31d262f15fb281ea7371a
Lekhana <lekhanamaridi444@gmail.com>
2025-11-27 12:43:15 +0530

    Login part done

--- src/main/java/com/jsp/book_my_ticket/service/UserService.java
+++ src/main/java/com/jsp/book_my_ticket/service/UserService.java
@@ -1,5 +1,15 @@
 package com.jsp.book_my_ticket.service;
 
-public interface UserService {
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.jsp.book_my_ticket.dto.LoginDto;
+import com.jsp.book_my_ticket.dto.UserDto;
+
+import jakarta.servlet.http.HttpSession;
+
+public interface UserService {
+	String register(UserDto userDto, BindingResult result);
+
+	String login(LoginDto dto, RedirectAttributes attributes,HttpSession session);
 }
