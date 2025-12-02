0ee09f55b94b4b9c073e61980a4facd4eff9a44c
Lekhana <lekhanamaridi444@gmail.com>
2025-12-02 12:28:26 +0530

    redis otp verification

--- src/main/java/com/jsp/book_my_ticket/controller/UserController.java
+++ src/main/java/com/jsp/book_my_ticket/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.jsp.book_my_ticket.dto.LoginDto;
@@ -20,7 +21,7 @@ public class UserController {
 
 	private final UserService userService;
 
-	@GetMapping({"/","/main"})
+	@GetMapping({ "/", "/main" })
 	public String loadMain() {
 		return "main.html";
 	}
@@ -31,8 +32,8 @@ public class UserController {
 	}
 
 	@PostMapping("/register")
-	public String register(@Valid UserDto userDto, BindingResult result) {
-		return userService.register(userDto, result);
+	public String register(@Valid UserDto userDto, BindingResult result, RedirectAttributes attributes) {
+		return userService.register(userDto, result, attributes);
 	}
 
 	@GetMapping("/login")
@@ -41,12 +42,22 @@ public class UserController {
 	}
 	
 	@PostMapping("/login")
-	public String login(LoginDto dto,RedirectAttributes attributes,HttpSession session) {
-		return userService.login(dto,attributes,session);
+	public String login(LoginDto dto, RedirectAttributes attributes, HttpSession session) {
+		return userService.login(dto, attributes, session);
 	}
 	
 	@GetMapping("/logout")
-	public String logout(HttpSession session,RedirectAttributes attributes) {
-		return userService.logout(session,attributes);
+	public String logout(HttpSession session, RedirectAttributes attributes) {
+		return userService.logout(session, attributes);
+	}
+
+	@GetMapping("/otp")
+	public String loadOtpPage() {
+		return "otp.html";
+	}
+
+	@PostMapping("/otp")
+	public String submitOtp(@RequestParam int otp, @RequestParam String email, RedirectAttributes attributes) {
+		return userService.submitOtp(otp, email, attributes);
 	}
 }
