c12829740d3d8144874f84ec9c8c53433b009bdc
Lekhana <lekhanamaridi444@gmail.com>
2025-12-04 13:16:24 +0530

    auth part completed

--- src/main/java/com/jsp/book_my_ticket/service/UserService.java
+++ src/main/java/com/jsp/book_my_ticket/service/UserService.java
@@ -1,9 +1,11 @@
 package com.jsp.book_my_ticket.service;
 
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.jsp.book_my_ticket.dto.LoginDto;
+import com.jsp.book_my_ticket.dto.PasswordDto;
 import com.jsp.book_my_ticket.dto.UserDto;
 
 import jakarta.servlet.http.HttpSession;
@@ -16,4 +18,10 @@ public interface UserService {
 	String logout(HttpSession session, RedirectAttributes attributes);
 	
 	String submitOtp(int otp, String email, RedirectAttributes attributes);
+	
+	String resendOtp(String email, RedirectAttributes attributes);
+
+	String forgotPassword(String email, RedirectAttributes attributes);
+
+	String resetPassword(PasswordDto passwordDto, BindingResult result, RedirectAttributes attributes, ModelMap map);
 }
