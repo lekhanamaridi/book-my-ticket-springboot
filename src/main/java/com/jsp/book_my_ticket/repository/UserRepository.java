214e054fe8790924c9937ff5c8acf1e64635c5b4
Lekhana <lekhanamaridi444@gmail.com>
2025-12-06 09:34:49 +0530

    Manage users

--- src/main/java/com/jsp/book_my_ticket/repository/UserRepository.java
+++ src/main/java/com/jsp/book_my_ticket/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.jsp.book_my_ticket.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jsp.book_my_ticket.entity.User;
@@ -13,4 +15,6 @@ public interface UserRepository extends JpaRepository<User, Long>{
 	User findByEmail(String email);
 	
 	boolean existsByMobile(Long mobile);
+	
+	List<User> findByRole(String string);
 }
