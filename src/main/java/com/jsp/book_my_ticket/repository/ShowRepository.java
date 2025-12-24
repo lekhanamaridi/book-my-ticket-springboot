dd11d81b4f877fd1b37e82af66b556a34d668056
Lekhana <lekhanamaridi444@gmail.com>
2025-12-24 11:41:56 +0530

    fixed issues

--- src/main/java/com/jsp/book_my_ticket/repository/ShowRepository.java
+++ src/main/java/com/jsp/book_my_ticket/repository/ShowRepository.java
@@ -1,9 +1,11 @@
 package com.jsp.book_my_ticket.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.jsp.book_my_ticket.entity.Movie;
 import com.jsp.book_my_ticket.entity.Screen;
 import com.jsp.book_my_ticket.entity.Show;
 
@@ -11,4 +13,7 @@ public interface ShowRepository extends JpaRepository<Show, Long> {
 
 	List<Show> findByScreen(Screen screen);
 
+	List<Show> findByShowDateAfter(LocalDate minusDays);
+
+	List<Show> findByMovieAndShowDateAfter(Movie movie, LocalDate minusDays);
 }
